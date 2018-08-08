8b902803a39f976785b33b32c4c9914fd88e8284
Chad Hietala <chadhietala@gmail.com>
2018-08-08 09:46:08 -0400

    Init

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-glimmer-components'
+  name: 'ember-glimmer-components',
+  config(env, appConfig) {
+    appConfig.EmberENV.FEATURES.EMBER_METAL_TRACKED_PROPERTIES = true;
+    appConfig.EmberENV.FEATURES.GLIMMER_COMPONENT_MANAGER_BOUNDS = true;
+    return this._super.config.apply(this, [env, appConfig]);
+  }
 };
