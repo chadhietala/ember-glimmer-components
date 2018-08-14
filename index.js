cdd225f21cf67067bb3b11bffadcb56d2fd68e0e
Steven Foote <sfoote@linkedin.com>
2018-08-14 13:38:08 -0600

    |WIP| Add babel-plugin-transform-tracked-properties
    Probably need to do some more safety checks before bolting on this
    transform. But this is a start.

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ module.exports = {
     appConfig.EmberENV.FEATURES.EMBER_METAL_TRACKED_PROPERTIES = true;
     appConfig.EmberENV.FEATURES.GLIMMER_COMPONENT_MANAGER_BOUNDS = true;
     return this._super.config.apply(this, [env, appConfig]);
+  },
+  included(parent) {
+    this._super.included.apply(parent);
+    const parentoptions = parent.options || {};
+    parentoptions.babel = parentoptions.babel || {};
+    parentoptions.babel.plugins = parentoptions.babel.plugins || [];
+
+    if (!this._registeredtrackedpropertiestransform) {
+      parentoptions.babel.plugins.push(require.resolve('babel-plugin-transform-tracked-properties'));
+      this._registeredtrackedpropertiestransform = true;
+    }
   }
 };
